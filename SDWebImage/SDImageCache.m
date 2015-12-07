f93894c8898f3da421a279f7acae91ca4301abf9
ruaho <liwei@ruaho.com>
2015-12-07 10:05:56 +0800

    Update SDImageCache.m
    修改图片缓存的日期

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -38,7 +38,7 @@ static NSString * categoryKey = @"IMG_CATEGORY=";
 
 
 
-static const NSInteger kDefaultCacheMaxCacheAge = 60 * 60 * 24 * 7; // 1 week
+static const NSInteger kDefaultCacheMaxCacheAge = 60 * 60 * 24 * 60; // 1 week
 // PNG signature bytes and data (below)
 static unsigned char kPNGSignatureBytes[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
 static NSData *kPNGSignatureData = nil;
